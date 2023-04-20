5cc96ec961d70cab013e3955d894aa8abbafa4b0
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-04-20 22:41:52 +0530

    Create tb.sv

---
+++ Day1/MUX_2to1/tb.sv
@@ -0,0 +1,28 @@
+// Code your testbench here
+// or browse Examples
+module tb();
+  
+  reg [7:0] a;
+  reg [7:0] b;
+  reg sel;
+  wire [7:0] out;
+  
+  mux2to1 mux1(.a(a), .b(b), .sel(sel), .out(out));
+  
+  integer i;
+  initial begin
+    for(i=0;i<10;i=i+1)begin
+      a = $random%8;
+      b = $random%8;
+      sel = $random%2;
+      #5;
+    end
+  end
+  
+    initial begin
+      $dumpfile("mux.vcd");
+      $dumpvars(0, tb);
+  end
+  
+endmodule
+      
