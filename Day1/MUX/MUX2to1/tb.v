c8dd3b0fa8bd400ab0dff932462a66f5487cf1a0
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-04-21 19:41:14 +0530

    Create tb.v

---
+++ Day1/MUX/MUX2to1/tb.v
@@ -0,0 +1,26 @@
+module tb();
+  
+  reg [7:0] a;
+  reg [7:0] b;
+  reg sel;
+  wire [7:0] out;
+  
+  mux2to1 mux1(.a(a), .b(b), .sel(sel), .out(out));
+  
+  integer i;
+  initial begin
+    for(i=0;i<10;i=i+1)begin
+      a = {$random}%255;
+      b = {$random}%255;
+      sel = {$random}%2;
+      #1;
+    end
+  end
+  
+    initial begin
+      $dumpfile("mux2to1.vcd");
+      $dumpvars(0, tb);
+  end
+  
+endmodule
+      
