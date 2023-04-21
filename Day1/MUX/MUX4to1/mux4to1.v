ba9a4862f5ae39a3fdb0f987491c8cb12c086ea7
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-04-21 22:36:05 +0530

    Update mux4to1.v

--- Day1/MUX/MUX4to1/mux4to1.v
+++ Day1/MUX/MUX4to1/mux4to1.v
@@ -3,10 +3,9 @@ module mux4to1(
   input wire [7:0] b,
   input wire [7:0] c,
   input wire [7:0] d,
-  input wire sel,
+  input wire [1:0] sel,
   output wire [7:0] out);
   
-  assign out = (sel == 2'b00)?a:(sel == 2'b01)?b:(sel == 2'b10)?c:d
+   assign out = sel[0]?(sel[1]?d:b):(sel[1]?c:a);
   
 endmodule
-  
