9d95790f67a4c393d892b0c1ad0b5ee7358df524
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-04-22 23:25:08 +0530

    Create dff.sv

---
+++ Day3/DFF/dff.sv
@@ -0,0 +1,32 @@
+module dff(
+  input logic d, 
+  input logic clk, 
+  input logic rst, 
+  output logic q_norst, 
+  output logic q_asyncrst, 
+  output logic q_syncrst);
+  
+  always @(posedge clk) begin
+    q_norst <= d;
+  end
+  
+  always@(posedge clk) begin
+    if (rst) begin
+      q_syncrst = 1'b0;
+    end
+    else begin
+      q_syncrst <=d;
+    end
+  end
+  
+  always@(posedge clk or rst) begin
+    if (rst) begin
+      q_asyncrst = 1'b0;
+    end
+    else begin
+      q_asyncrst <= d;
+    end
+  end
+endmodule
+        
+        
