c83f8b5a6b75a2b27ebd1b98f5255284a3293b0b
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-04-23 18:13:36 +0530

    Update tb.sv

--- Day3/DFF/tb.sv
+++ Day3/DFF/tb.sv
@@ -1,3 +1,4 @@
+`timescale 1ns/1ps
 module tb();
   logic d;
   logic clk; 
@@ -6,22 +7,40 @@ module tb();
   logic q_asyncrst; 
   logic q_syncrst;
   
+  //Instantiate the DUT
+  dff dff1(.d(d), .clk(clk),.rst(rst),.q_norst(q_norst),.q_asyncrst(q_asyncrst),.q_syncrst(q_syncrst));
+  
   integer i;
   initial begin
-    for(i=0;i<10;i=i+1) begin
+    rst = 1'b0;
+    #2;
+    rst = 1'b1;
+    #0.5;
+    rst = 1'b0;
+    #1; rst = 1'b1;
+    #0.75; rst = 1'b0;
+    #1.25; rst = 1'b1;
+    #1.75 rst = 1'b0;
+  end
+  initial begin
+    for(i=0;i<40;i=i+1) begin
       d = $urandom_range(1'b0,1'b1);
-      rst = $urandom_range(1'b0,1'b1);
-      #1;
+      #0.5;
     end
   end
   
   initial begin
-    clk = 0;
-    #10 $finish;
+    clk = 1;
+    #20 $finish;
   end
   
   always begin
-    #1 clk = ~clk;
+    #0.5 clk = ~clk;
+  end
+  
+    initial begin
+      $dumpfile("dff.vcd");
+      $dumpvars(0, tb);
   end
   
 endmodule
