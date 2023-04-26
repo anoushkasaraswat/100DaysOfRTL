aedd652c51767015de71f018662a339ab3b28d28
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-04-27 02:21:12 +0530

    Update moore_fsm.sv

--- Moore FSM 1011/moore_fsm.sv
+++ Moore FSM 1011/moore_fsm.sv
@@ -1,11 +1,23 @@
+// Code your design here
 module moore_1011(
   input logic clk,
   input logic rst,
   input logic input_stream,
   output logic out);
   
+  logic [3:0] pattern_dff;
+  logic [3:0] nxt_pattern;
+  
+  assign nxt_pattern = {pattern_dff[2:0],input_stream};
+  
   always @(posedge clk) begin
     if (rst) begin
-      out <= 1'b0
+      pattern_dff <= 4'h0;
     end
-    else
+    else begin
+      pattern_dff <= nxt_pattern;
+      end
+  end
+  
+  assign out = (pattern_dff == 4'b1011)?1:0;
+endmodule
