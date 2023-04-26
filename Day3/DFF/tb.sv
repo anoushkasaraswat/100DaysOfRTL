b7a45615d26f675203fa6189bcdb4e226f709aa8
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-04-27 01:58:15 +0530

    Update tb.sv

--- Day3/DFF/tb.sv
+++ Day3/DFF/tb.sv
@@ -21,10 +21,21 @@ module tb();
     #0.75; rst = 1'b0;
     #1.25; rst = 1'b1;
     #1.75 rst = 1'b0;
+/*    rst <= 1'b0; //Non-blocking synchronous reset doesn't reset the output in the same clock edge since the if condition is evaluated in active region and NB assignment happens in NBA region. However, the waveform for async reset stays the same as blocking statement since the NBA assignment retriggers the always block and more active events.
+    #2;
+    rst <= 1'b1;
+    #0.5;
+    rst <= 1'b0;
+    #1; rst <= 1'b1;
+    #0.75; rst <= 1'b0;
+    #1.25; rst <= 1'b1;
+    #1.75 rst <= 1'b0;*/  
   end
   initial begin
+    d <= 1'b1;
+    #3;
     for(i=0;i<40;i=i+1) begin
-      d = $urandom_range(1'b0,1'b1);
+      d <= $urandom_range(1'b0,1'b1);
       #0.5;
     end
   end
@@ -44,3 +55,4 @@ module tb();
   end
   
 endmodule
+
