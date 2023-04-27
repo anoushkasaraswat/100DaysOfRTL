095b95960e80956a6f150976c06219c48aa10730
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-04-27 11:58:57 +0530

    Update shift_reg.sv

--- SIPO/shift_reg.sv
+++ SIPO/shift_reg.sv
@@ -4,7 +4,23 @@ module shift_reg(
   input logic rst,
   output logic [3:0] out);
   
+  logic [4:0] out_ff;
+  logic [4:0] next_out;
+  
+  //Better visibility and representation
+  assign next_out = {in, out_ff[3:1]};
+  assign out = out_ff;
+  
   always @(posedge clk) begin
+    if (rst) begin
+      out_ff <= 4'b0000;
+    end
+    else begin
+      out_ff <= next_out;
+    end
+  end  
+
+  /*always @(posedge clk) begin
     if (rst) begin
       out <= 4'b0000;
     end
@@ -14,6 +30,7 @@ module shift_reg(
       out[1] <= out[2];
       out[0] <= out[1];
     end
-  end
+  end*/
+  
 endmodule
       
