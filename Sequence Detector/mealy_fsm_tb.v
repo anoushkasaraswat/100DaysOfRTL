0afe8d7380877c03be8a47f6771132b64fde406d
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-04-28 23:17:35 +0530

    Create mealy_fsm_tb.v

---
+++ Sequence Detector/mealy_fsm_tb.v
@@ -0,0 +1,48 @@
+// Code your testbench here
+// or browse Examples
+module tb();
+  reg clk;
+  reg rst;
+  reg in;
+  wire out;
+  
+  mealy_1011 m(.clk(clk), .rst(rst), .in(in),.out(out));
+  
+  initial begin
+    clk = 1;
+    #40 $finish;
+  end
+  
+  always begin
+    #1 clk = ~clk;
+  end
+  
+  initial begin
+    rst <= 1'b1;
+    in <= 1'b0;
+    @(posedge clk);
+    rst <= 1'b0;
+    @(posedge clk);
+    in <= 1'b1;
+    @(posedge clk);
+    @(posedge clk);
+    in <= 1'b0;
+    @(posedge clk);
+    in <= 1'b1;    
+    @(posedge clk);
+    in <= 1'b0;
+    in <= 1'b1;
+    @(posedge clk);
+    @(posedge clk);
+    in <= 1'b0;
+    @(posedge clk);
+    in <= 1'b1;    
+    @(posedge clk);
+    in <= 1'b1;
+  end
+  
+  initial begin
+      $dumpfile("dff.vcd");
+      $dumpvars(0, tb);
+  end
+endmodule
