7a32bf51059e23ac37c7b0a986bf70c67dd2219c
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-04-30 00:55:25 +0530

    Update moore_fsm.sv

--- Sequence Detector/moore_fsm.sv
+++ Sequence Detector/moore_fsm.sv
@@ -7,57 +7,50 @@ module moore_1011(
   typedef enum logic [2:0] 
   {
     S0, S1, S2, S3, S4
-  } state;
+  } st;
   
-  state st;
+  //localparam S0 = 2'b00, S1 = 2'b01; S2 = 2'b10, S3 = 2'b11;
+  st pst; //present state
+  logic [2:0] nst; // next state
   
-  always@(posedge clk) begin
+  // combinational next state logic
+  always@(*) begin
+    case(pst)
+      S0: begin
+        nst = in?S1:S0;
+      end
+      S1: begin
+        nst = in?S1:S2;
+      end
+      S2: begin
+        nst = in?S3:S0;
+      end
+      S3: begin
+        nst = in?S4:S2;
+      end       
+      S4: begin
+        nst = in?S1:S2;
+      end
+    endcase
+  end
+  
+//present state sequential logic
+  always@(posedge clk or posedge rst) begin 
     if (rst) begin
-      st <= S0;
+      pst <= S0;
+    end
+    else begin
+      pst <= nst;
+    end
+  end
+
+//registered output
+  always@(posedge clk or posedge rst) begin
+    if (rst) begin
       out <= 1'b0;
     end
     else begin
-      case(st)
-        S0: begin
-          out <= 1'b0;
-          if (in) begin
-            st <= S1;
-          end
-        end
-        S1: begin
-          out <= 1'b0;
-          if (~in) begin
-            st <= S2;
-          end
-        end
-        S2: begin
-          out <= 1'b0;
-          if (in) begin
-            st <= S3;
-          end
-          else begin
-            st <= S0;
-          end
-        end
-        S3: begin
-          out <= 1'b0;
-          if (in) begin
-            st <= S4;
-          end
-          else begin
-            st <= S2;
-          end
-        end        
-        S4: begin
-          out <= 1'b1;
-          if (in) begin
-            st <= S1;
-          end
-          else begin
-            st <= S2;
-          end
-        end
-      endcase
+      out <= (pst==S4);
     end
   end
 endmodule
