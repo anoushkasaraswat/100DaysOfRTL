6ea27a1d0c3e76597e83170a0f5b359a19b82ea4
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-05-02 20:10:48 +0530

    Create fixed_pri_arbiter.sv

---
+++ Fixed Priority Arbiter/fixed_pri_arbiter.sv
@@ -0,0 +1,13 @@
+module fixed_pri_arbiter#(parameter REQUESTERS = 4)(
+  input wire [REQUESTERS-1:0] req_i,
+  output wire [REQUESTERS-1:0] grant_i);
+  
+  assign grant_i[0] = req_i[0]?1:0;
+  
+  generate
+    genvar i;
+    for(i=1;i<REQUESTERS;i=i+1) begin
+      assign grant_i[i] = req_i[i] & ~(|req_i[i-1:0]);
+    end
+  endgenerate
+endmodule
