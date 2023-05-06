057cc5b8d0c321a60dbd36e315871d245492e796
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-05-06 23:36:34 +0530

    Update rr_arbiter.v

--- Round Robin Arbiter/rr_arbiter.v
+++ Round Robin Arbiter/rr_arbiter.v
@@ -1,3 +1,4 @@
+// Code your design here
 module rr_arbiter#(parameter NUM_REQS=4)(
   input wire clk,
   input wire rst,
@@ -5,21 +6,29 @@ module rr_arbiter#(parameter NUM_REQS=4)(
   output wire [NUM_REQS-1:0] grant_o); 
   
   reg [NUM_REQS-1:0] mask;
-  reg [NUM_REQS-1:0] next_mask;
+  wire [NUM_REQS-1:0] next_mask;
   wire [NUM_REQS-1:0] mask_grant;
   wire [NUM_REQS-1:0] unmask_grant;
   wire [NUM_REQS-1:0] mask_req_i;
   
-  always@(*) begin
+  /*always@(*) begin
     next_mask = mask;
     if (grant_o[0]) next_mask = 4'b1110;
     else if (grant_o[1]) next_mask = 4'b1100;
     else if (grant_o[2]) next_mask = 4'b1000;
     else if (grant_o[3]) next_mask = 4'b0000;
-  end
+  end*/
   
-  always@(posedge clk or posedge rst) begin
-    if (rst) begin
+  genvar i;
+  generate 
+    assign next_mask[0] = 1'b0;
+    for(i=1;i<NUM_REQS;i=i+1)begin
+      assign next_mask[i] = grant_o[i-1] | next_mask[i-1];
+    end
+  endgenerate
+  
+  always@(posedge clk or negedge rst) begin
+    if (~rst) begin
       mask <= {NUM_REQS{1'b1}};
     end
     else begin
@@ -47,13 +56,4 @@ module fp_arbiter#(parameter NUM_REQS=4)(
   for(i=1;i<NUM_REQS;i=i+1) begin
     assign grant_o[i] = req_i[i] & (~|req_i[i-1:0]);
   end
-endmodule
-  
-  
-  
-  
-  
-  
-  
-                
-    
+endmodule     
