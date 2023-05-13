cb6c3a6d0ab672a537c61e59598ce6ddf571c749
Anoushka Saraswat <39295663+anoushkasaraswat@users.noreply.github.com>
2023-05-14 00:15:31 +0530

    Update fifo.sv

--- FIFO/fifo.sv
+++ FIFO/fifo.sv
@@ -1,3 +1,4 @@
+// Code your design here
 module fifo#(parameter DEPTH = 16,
              parameter WIDTH = 16)(
   	input logic clk,
@@ -18,7 +19,7 @@ module fifo#(parameter DEPTH = 16,
   logic [DEPTH-1:0][WIDTH-1:0] mem_queue;
   logic [addr_bits:0] rd_ptr, wr_ptr, max_index_ptr, min_index_ptr;
 
-  assign max_index_ptr = {addr_bits+1{1'b1}};
+  assign max_index_ptr = {1'b1,{addr_bits{1'b0}}};
   assign min_index_ptr = {addr_bits+1{1'b0}};
   
   assign fifo_full = (rd_ptr[addr_bits] != wr_ptr[addr_bits] ) && (rd_ptr[addr_bits-1:0] == wr_ptr[addr_bits-1:0] );
@@ -32,18 +33,40 @@ module fifo#(parameter DEPTH = 16,
     else begin
       case({push_i,pop_i})
         PUSH_ONLY: begin
-          wr_ptr <= wr_ptr + {{addr_bits{1'b0}},1'b1};
-          mem_queue[wr_ptr] <= push_data_i;
+          mem_queue[wr_ptr] <= push_data_i;          
+          if (wr_ptr == max_index_ptr) begin
+            wr_ptr <= {addr_bits+1{1'b0}};
+          end
+          else begin
+            wr_ptr <= wr_ptr + {{addr_bits{1'b0}},1'b1};
+          end
         end
+        
         POP_ONLY: begin 
-          rd_ptr <= rd_ptr + {{addr_bits{1'b0}},1'b1};
           pop_data_i <= mem_queue[rd_ptr];
+          if (rd_ptr == max_index_ptr) begin
+            rd_ptr <= {addr_bits+1{1'b0}};
+          end
+          else begin
+          	rd_ptr <= rd_ptr + {{addr_bits{1'b0}},1'b1};
+          end
         end
+        
         PUSH_AND_POP: begin
-          wr_ptr <= wr_ptr + {{addr_bits{1'b0}},1'b1};
-          rd_ptr <= rd_ptr + {{addr_bits{1'b0}},1'b1};
           mem_queue[wr_ptr] <= push_data_i;
           pop_data_i <= mem_queue[rd_ptr];
+          if (wr_ptr == max_index_ptr) begin
+            wr_ptr <= {addr_bits+1{1'b0}};
+          end
+          else begin
+            wr_ptr <= wr_ptr + {{addr_bits{1'b0}},1'b1};
+          end    
+          if (rd_ptr == max_index_ptr) begin
+            rd_ptr <= {addr_bits+1{1'b0}};
+          end
+          else begin
+          	rd_ptr <= rd_ptr + {{addr_bits{1'b0}},1'b1};
+          end        
         end
       endcase
     end
